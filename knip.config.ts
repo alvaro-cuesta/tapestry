8bcc9e6e1b5c01b41ced8cfb2b1e284d27b63b4f
Álvaro Cuesta <1827495+alvaro-cuesta@users.noreply.github.com>
2025-08-22 00:33:57 +0200

    lint: fix knip

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,11 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-  entry: ['src/main.tsx'],
+  entry: [
+    'src/main.tsx',
+    // used by @vite-pwa/assets-generator
+    'pwa-assets.config.ts',
+  ],
   project: ['**/*.{js,jsx,ts,tsx}'],
 };
 
